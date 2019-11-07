1c7219169d4e6321aa18673bb9263f57173319a1
njzhan <t07087@mail.fcu.edu.tw>
2019-11-07 13:52:01 +0800

    修改answers

--- ExcelToJson.py
+++ ExcelToJson.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import xlrd
 import json
 
@@ -12,10 +13,18 @@ data = []
 for v in range(1, n_rows):
     # 每一行資料形成一個列表
     values = table.row_values(v)
+
+    answers = []
+    for num in range(1, 5):  # 1~4
+        try:
+            answers.append(str(int(values[num])))
+        except ValueError:
+            answers.append(str(values[num]))
     # 列表形成字典
     data.append({'question': values[0],
-                 'answers': [values[1], values[2], values[3], values[4]],
+                 'answers': answers,
                  'correct': int(values[5])
                  })
 with open('./quest.json', 'w') as f:
     json.dump(data, f)
+print('轉換完成')
